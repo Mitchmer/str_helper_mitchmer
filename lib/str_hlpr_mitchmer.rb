48f6a6cffd2c417ae6126c45f5aebcb4024d31cc
Mitch Merrell <mitchmerrell88@gmail.com>
2018-04-18 10:57:42 -0600

    added broken testing

--- lib/str_hlpr_mitchmer.rb
+++ lib/str_hlpr_mitchmer.rb
@@ -1,5 +1,24 @@
 require "str_hlpr_mitchmer/version"
 
 module StrHlprMitchmer
-  # Your code goes here...
+  def self.reversify(str)
+    str.split('').reverse.join('')
+  end
+
+  def self.casify(str)
+    to_case = [:upcase, :downcase]
+    arr = str.split('')
+    arr.each_with_index do |letter, i|
+      this_case = to_case[rand(2)]
+      arr[i] = letter.send(this_case)
+    end
+  arr.join('')
+  end
+
+  def self.spacify(str, spaces = 0)
+    new_str = str
+    spaces.times do
+      new_str = new_str.split('').join('')
+    end
+  end
 end
